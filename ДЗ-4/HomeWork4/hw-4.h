dcbb6626dc6e2917d29c26651a0a2477587d74c2
Yakov <yaxa499@gmail.com>
2025-10-06 19:24:39 +0300

    Add struct Employee

---
+++ ДЗ-4/HomeWork4/hw-4.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+/// <summary>
+/// Данные о сотруднике компании
+/// </summary>
+struct Employee
+{
+	/// <summary>
+	/// Имя сотрудника
+	/// </summary>
+	std::string name;
+
+	/// <summary>
+	/// Идентификационный номер 
+	/// </summary>
+	int employee_id;
+
+	/// <summary>
+	/// Должность
+	/// </summary>
+	std::string position;
+
+	/// <summary>
+	/// Базовый оклад
+	/// </summary>
+	double salary;
+
+	/// <summary>
+	/// Стаж работы в компании(лет)
+	/// </summary>
+	int years_of_service;
+
+	/// <summary>
+	/// Умения
+	/// </summary>
+	std::vector<std::string> skills;
+
+	/// <summary>
+	/// Конструктор
+	/// </summary>
+	/// <param name="name">Имя сотрудника</param>
+	/// <param name="employee_id">Идентификационный номер </param>
+	/// <param name="position">Должность</param>
+	/// <param name="years_of_service">Стаж работы в компании(лет)</param>
+	Employee(std::string name, int employee_id, std::string position, int years_of_service)
+	{
+		this->name = name;
+		this->employee_id = employee_id;
+		this->position = position;
+		this->years_of_service = years_of_service;
+	}
+};
---
+++ ДЗ-4/HomeWork4/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "hw-4.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+}
